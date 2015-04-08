fd96b90fa816bdb1ea91eec1b3e06bac5059c0b3
Aritra Dhar <aritra.dhar7@gmail.com>
2015-04-08 11:43:29 +0530

    Init

---
+++ src/com/Server/MainServlet.java
@@ -0,0 +1,257 @@
+package com.Server;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.math.BigInteger;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.crypto.EvenGoldreichLempel;
+import com.util.Utils;
+
+
+/**
+ * Servlet implementation class MainServlet
+ */
+@WebServlet("/MainServlet")
+public class MainServlet extends HttpServlet 
+{
+	private static final long serialVersionUID = 1L;
+
+	public static BigInteger N,D,E;	
+	int n_msg = 200;
+	public static String keyJson;
+	public static String[] base64Keys;
+	public static BigInteger[] bigIntegerKeys;
+	public static String signKey, verifyKey;
+
+	public static boolean loaded = false;
+
+	public MainServlet() throws IOException 
+	{
+		super();
+
+		BufferedReader br = new BufferedReader(new FileReader("C:\\KeyBase\\Key.json"));
+		String s = "";
+		StringBuffer sb = new StringBuffer("");
+
+		while((s = br.readLine()) != null)
+		{
+			sb = sb.append(s).append("\n");
+		}
+
+		keyJson = sb.toString();
+		br.close();
+
+		// parse json from the key file
+
+		JSONObject jObject = new JSONObject(keyJson);
+		JSONArray jArray = jObject.getJSONArray("ImageBase64KeyMap");
+		base64Keys = new String[jArray.length()];
+		bigIntegerKeys = new BigInteger[jArray.length()];
+
+		for(int i = 0; i < jArray.length(); i++)
+		{
+			JSONObject ob = (JSONObject) jArray.get(i);
+			//System.out.println(ob.get("imgFile").toString());
+
+			base64Keys[i] = (String) ob.get("key");
+			bigIntegerKeys[i] = Utils.stringToBigInteger(base64Keys[i]);
+
+		}
+
+		//System.out.println(bigIntegerKeys[10]);
+		System.out.println("Started");
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse res) throws ServletException, IOException 
+	{
+		// TODO Auto-generated method stub
+		String response = "GET request not supported";
+		res.getOutputStream().write(response.getBytes());
+		res.getOutputStream().flush();
+		res.getOutputStream().close();
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse res) throws ServletException, IOException 
+	{
+		res.setContentType("text/plain");
+
+		String flag = request.getParameter("flag");
+
+		String ip = request.getRemoteAddr();
+		int port = request.getRemotePort();
+		System.out.println("Request from");
+		System.out.println("IP : " + ip);
+		System.out.println("PORT : " + port);
+		System.out.println("FLAG : " + flag);
+
+		try 
+		{
+			System.out.println("Loaded key file " + loaded);
+
+			if(!loaded)
+			{
+				BufferedReader brN = new BufferedReader(new FileReader("C:\\KeyBase\\N.key"));
+				BufferedReader brD = new BufferedReader(new FileReader("C:\\KeyBase\\D.key"));
+				BufferedReader brE = new BufferedReader(new FileReader("C:\\KeyBase\\E.key"));
+
+				BufferedReader brSignKey = new BufferedReader(new FileReader("C:\\KeyBase\\EC_SK.key"));
+				BufferedReader brVerifyKey = new BufferedReader(new FileReader("C:\\KeyBase\\EC_PK.key"));
+				
+				N = new BigInteger(brN.readLine());
+				D = new BigInteger(brD.readLine());
+				E = new BigInteger(brE.readLine());
+				
+				signKey = brSignKey.readLine();
+				verifyKey = brVerifyKey.readLine();
+
+				brN.close();
+				brD.close();
+				brE.close();
+				brSignKey.close();
+				brVerifyKey.close();
+
+				loaded = true;
+			}
+		} 
+
+		catch (Exception e) 
+		{
+			System.out.println("Exception in key genration");
+
+		}
+
+		//System.out.println("N " + N);
+		//System.out.println("D " + D);
+		//System.out.println("E " + E);
+
+		//destroy all styates
+		if(flag.equalsIgnoreCase("endSession"))
+		{
+			boolean end = UserList.endSession(ip, port);
+			if(!end)
+			{
+				String response = "invalid_rerquest";
+				res.getOutputStream().write(response.getBytes());
+				res.getOutputStream().flush();
+				res.getOutputStream().close();
+				return;
+			}
+			else
+			{
+				String response = "session_end";
+				res.getOutputStream().write(response.getBytes());
+				res.getOutputStream().flush();
+				res.getOutputStream().close();
+				return;
+
+			}
+		}
+
+		//handshake -> state 1
+		//send RSA n and e and Elliptic curve public key for signature
+		if(flag.equalsIgnoreCase("handshake"))
+		{
+			if(UserList.getState(ip, port) > 0)
+			{
+				String response = "wrong_state";
+				res.getOutputStream().write(response.getBytes());
+				res.getOutputStream().flush();
+				res.getOutputStream().close();
+				return;
+			}
+
+			UserList.setState(request.getRemoteAddr(), request.getRemotePort(), 1);
+			String response = N.toString().concat("\n").concat(E.toString()).concat("\n").concat(verifyKey);
+			res.getOutputStream().write(response.getBytes());
+			res.getOutputStream().flush();
+			res.getOutputStream().close();
+		}
+
+		if(flag.equalsIgnoreCase("otKeys"))
+		{
+			BigInteger[] X =  EvenGoldreichLempel.generateRandomMsg(n_msg, 4);
+			boolean status = UserList.putX(ip, port, X);
+
+			if(!status)
+			{
+				String response = "invalid_request";
+				res.getOutputStream().write(response.getBytes());
+				res.getOutputStream().flush();
+				res.getOutputStream().close();
+			}
+
+			JSONObject X_Json = Utils.putInJSON(X);
+
+			res.getOutputStream().write(X_Json.toString(2).getBytes());
+			res.getOutputStream().flush();
+			res.getOutputStream().close();
+		}
+
+		if(flag.equalsIgnoreCase("otQuery"))
+		{
+			BigInteger query = new BigInteger(request.getParameter("query"));
+
+			//for(int i = 0; i < base64Keys.length; i++)
+			//{
+			BigInteger V = query;
+			//System.out.println("V " + V);
+			UserList.putQuery(ip, port, query);
+			BigInteger[] X = UserList.getX(ip, port);
+
+			if(X == null)
+			{
+				String response = "invalid_state";
+				res.getOutputStream().write(response.getBytes());
+				res.getOutputStream().flush();
+				res.getOutputStream().close();
+			}
+			//System.out.println("X " + X[0]);
+
+			BigInteger[] enc = EvenGoldreichLempel.sendObliviousEnc(V, n_msg, X, D, N, bigIntegerKeys);
+
+			JSONObject enc_Json = Utils.putInJSONEnc(enc);
+			
+			FileWriter wr = new FileWriter("C:\\logs\\" + Utils.makeAddress(ip, port) + "_log.txt", true);
+			wr.append(enc_Json.toString(2));
+			wr.close();
+			
+			//Utils.LZMA_ZIP("C:\\logs\\" + Utils.makeAddress(ip, port) + "_log.txt", "C:\\logs\\" + Utils.makeAddress(ip, port) + "_lzma.xz");
+
+			res.getOutputStream().write(enc_Json.toString(2).getBytes());
+			res.getOutputStream().flush();
+			res.getOutputStream().close();
+			//}
+
+		}
+
+		if(flag.equalsIgnoreCase("getkeys"))
+		{
+			res.getOutputStream().write(keyJson.getBytes());
+			res.getOutputStream().flush();
+			res.getOutputStream().close();
+		}
+
+		else
+		{
+			String response = "wrong_flag_type";
+			res.getOutputStream().write(response.getBytes());
+			res.getOutputStream().flush();
+			res.getOutputStream().close();
+
+		}
+
+	}
+}
+
---
+++ src/com/Server/UserList.java
@@ -0,0 +1,114 @@
+//*************************************************************************************
+//*********************************************************************************** *
+//author Aritra Dhar 																* *
+//Research Engineer																  	* *
+//Xerox Research Center India													    * *
+//Bangalore, India																    * *
+//--------------------------------------------------------------------------------- * * 
+///////////////////////////////////////////////// 									* *
+//The program will do the following:::: // 											* *
+///////////////////////////////////////////////// 									* *
+//version 1.0 																		* *
+//*********************************************************************************** *
+//*************************************************************************************
+
+
+package com.Server;
+
+import java.math.BigInteger;
+import java.util.concurrent.ConcurrentHashMap;
+
+import com.util.Utils;
+
+public class UserList 
+{
+	// keep address (IP |$| PORT) and the state
+	public static ConcurrentHashMap<String, Integer> userStateMap = new ConcurrentHashMap<>();
+	
+	// keep address (IP |$| PORT) and randomly generated X
+	public static ConcurrentHashMap<String, BigInteger[]> userXval = new ConcurrentHashMap<String, BigInteger[]>();
+	
+	//keep address (IP |$| PORT) and user query
+	public static ConcurrentHashMap<String, BigInteger> userQueryMap = new ConcurrentHashMap<String, BigInteger>();
+	
+	
+	public static void setState(String ip, int port, int state)
+	{
+		String address = Utils.makeAddress(ip, port);
+		
+		if(!userStateMap.containsKey(address))
+		{
+			userStateMap.put(address, state);
+		}
+		
+		else
+		{
+			userStateMap.put(address, state);
+		}
+	}
+	
+	public static int getState(String ip, int port)
+	{
+		String address = Utils.makeAddress(ip, port);
+		
+		return userStateMap.containsKey(address) ? userStateMap.get(address) : -1;
+	}
+	
+	public static boolean endSession(String ip, int port)
+	{
+		String address = Utils.makeAddress(ip, port);
+		
+		if(!userStateMap.containsKey(address))
+			return false;
+		
+		userStateMap.remove(address);
+		userXval.remove(address);
+		userQueryMap.remove(address);
+		
+		return true;
+	}
+	
+	public static boolean putX(String ip, int port, BigInteger[] X)
+	{
+		String address = Utils.makeAddress(ip, port);
+		
+		if(!userXval.containsKey(address) && !userStateMap.containsKey(address))
+			return false;
+		
+		userStateMap.put(address, 2);
+		userXval.put(address, X);
+		return true;
+	}
+	
+	public static BigInteger[] getX(String ip, int port)
+	{
+		String address = Utils.makeAddress(ip, port);
+		
+		if(!userXval.containsKey(address) && userStateMap.get(address) != 2)
+			return null;
+		
+		return userXval.get(address);
+	}
+	
+	public static boolean putQuery(String ip, int port, BigInteger query)
+	{
+		String address = Utils.makeAddress(ip, port);
+		
+		if(!userQueryMap.containsKey(address))
+			return false;
+		
+		userStateMap.put(address, 3);
+		userQueryMap.put(address, query);
+		return false;
+	}
+	
+	public static BigInteger getQuery(String ip, int port)
+	{
+		String address = Utils.makeAddress(ip, port);
+		
+		if(!userQueryMap.containsKey(address))
+			return null;
+		
+		return userQueryMap.get(address);
+	}
+}
